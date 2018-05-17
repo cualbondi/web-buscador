6daf63569c344760bb3c2fd5704e0e0880bdfe8b
Federico Marcos <marcosfede@gmail.com>
2018-05-17 00:04:44 -0300

    vue material

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
-Vue.config.productionTip = false;
+// TODO: Import only the components that are used instead of everything
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount('#app')
