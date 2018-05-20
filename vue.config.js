2f7c5c9c5a9e5d4e9dcacf71c453220e7bd5aa46
Federico Marcos <marcosfede@gmail.com>
2018-05-20 17:13:29 -0300

    bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+const production = process.env.NODE_ENV === 'production'
+
+const developmentPlugins = []
+const productionPlugins = [
+  // bundle analyzer
+  new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    reportFilename: 'report.html',
+    openAnalyzer: true,
+  }),
+]
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: production ? productionPlugins : developmentPlugins
+  }
 }
