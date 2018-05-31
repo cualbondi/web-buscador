d3ecbe3ad8020ec52a779881c1661f71efe01ee2
Julian Perelli <jperelli@gmail.com>
2018-05-31 02:34:49 -0300

    add sentry config

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,15 @@ import './registerServiceWorker'
 import 'vuetify/dist/vuetify.min.css'
 import { isProd, GA_KEY } from '@/config'
 
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
+if (isProd) {
+  Raven
+    .config('http://67b7a80534a5442cb7dabed8033a7d65@sentry.cualbondi.com.ar/5')
+    .addPlugin(RavenVue, Vue)
+    .install()
+}
+
 // TODO: Import only the components that are used instead of everything
 // TODO: use a stylus loader to tree-shake unused css
 
