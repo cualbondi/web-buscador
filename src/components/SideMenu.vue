c98eaf7b8004862f8a892bb42719e6d1a6d76c4f
Julian Perelli <jperelli@gmail.com>
2018-06-18 23:25:40 -0300

    fix #7 add sidebar menu items

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -1,4 +1,4 @@
-<template>  
+<template>
     <v-navigation-drawer
       class="navigation"
       v-model="sideMenuOpen"
@@ -10,21 +10,57 @@
           <img :src="logo" />
         </v-list-tile>
       </v-list>
+      <v-list>
+        <v-list-tile>
+          <v-select
+            class="citySelect"
+            :items="ciudades"
+            v-model="ciudad"
+            item-text="nombre"
+            item-value="slug"
+            label="Ciudad"
+            single-line
+            auto
+            prepend-icon="map"
+            hide-details
+          ></v-select>
+        </v-list-tile>
+        <v-list-tile @click="openExternalLink(`https://cualbondi.com.ar/${ciudad.slug}/`)">
+          <div class="v-input__prepend-outer"><div class="v-input__icon v-input__icon--prepend"><v-icon>location_city</v-icon></div></div>
+          Ver mas informaci&oacute;n
+        </v-list-tile>
+      </v-list>
       <v-list class="pt-0" dense>
         <v-divider></v-divider>
       </v-list>
-      <v-list-tile>
-        <v-slider
-          @change="setRadius"
-          v-model="radius"
-          prepend-icon="directions_walk"
-          :persistent-hint="true"
-          :min="200"
-          :max="800"
-          :step="100"
-          :ticks="true"
-        />
-      </v-list-tile>
+      <v-list>
+        <v-list-tile>
+          <v-slider
+            @change="setRadius"
+            v-model="radius"
+            prepend-icon="directions_walk"
+            :persistent-hint="true"
+            :hint="'100'"
+            :min="200"
+            :max="800"
+            :step="100"
+            :ticks="true"
+          />
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+      </v-list>
+      <v-list>
+        <v-list-tile @click="openExternalLink(`https://cualbondi.com.ar/agradecimientos/`)">
+          <div class="v-input__prepend-outer"><div class="v-input__icon v-input__icon--prepend"><v-icon>star</v-icon></div></div>
+          Top usuarios
+        </v-list-tile>
+        <v-list-tile @click="openExternalLink(`https://spreadsheets.google.com/spreadsheet/viewform?formkey=dFJERlREQjUyaE1iSkFDYnhOa2Nnbmc6MQ+`, true)">
+          <div class="v-input__prepend-outer"><div class="v-input__icon v-input__icon--prepend"><v-icon>edit</v-icon></div></div>
+          Sugerencias
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
 </template>
 
@@ -42,9 +78,40 @@ export default class Home extends Vue {
   set sideMenuOpen(value) {
     this.$store.dispatch('setSideMenu', value)
   }
-
   public setRadius(value: number) {
     this.$store.dispatch('setRadius', value)
   }
+  public openExternalLink(href: string, inTab: boolean) {
+    window.open(href, inTab ? '_blank' : '_self');
+  }
+  get ciudad() {
+    return this.$store.getters.getCiudad
+  }
+  set ciudad(ciudadSlug) {
+    console.log(ciudadSlug)
+    this.$store.dispatch('setCiudad', ciudadSlug)
+  }
+  get ciudades() {
+    return this.$store.getters.getCiudades
+  }
 }
 </script>
+
+<style lang="scss">
+.theme--light .v-text-field .v-input__slot:before, .application .theme--light.v-text-field .v-input__slot:before,
+.theme--light .v-text-field:not(.v-input--has-state):hover .v-input__slot:before, .application .theme--light.v-text-field:not(.v-input--has-state):hover .v-input__slot:before,
+.v-text-field .v-input__slot:after
+{
+  background-color: transparent !important;
+}
+.navigation {
+  .v-input {
+    font-size: 14px;
+  }
+  .v-slider__thumb {
+    width: 50px;
+    height: 50px;
+    left: -25px;
+  }
+}
+</style>
