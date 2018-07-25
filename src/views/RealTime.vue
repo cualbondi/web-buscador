1fd044f076c7b8cd621a70d8cf2db2cd9e165767
Federico Marcos <marcosfede@gmail.com>
2018-07-24 23:30:29 -0300

    subscription test

--- src/views/RealTime.vue
+++ src/views/RealTime.vue
@@ -10,18 +10,15 @@ export default class Live extends Vue {
 
     mounted(){
         const sock = new WebSocket("ws://localhost:8084/subscribe")
-        sock.onmessage = (event: MessageEvent) => {
-            console.log('recieved ', event.data)
-        }
         sock.onopen = () => {
             const message = {
-                position: {
-                    lat: 1,
-                    lng: 2,
-                },
-                recorridos: [1,2,3]
+                position: 'POINT (-38.7406530000000018 -62.2151870000000002)',
+                recorridos: [0,1,2,3]
             }
             sock.send(JSON.stringify(message))
+        }
+        sock.onmessage = (event: MessageEvent) => {
+            console.log('recieved ', event.data)
         }
     }
 }
