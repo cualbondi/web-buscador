9b4f6b0b76c8cf3d54f2a5a97a5e4fdbb1cfcaea
Julian Perelli <jperelli@gmail.com>
2018-11-16 13:52:56 -0300

    fix https on localhost problem

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ export const isProd = process.env.NODE_ENV === 'production'
 export const API_URL = process.env.VUE_APP_API_URL
   ? process.env.VUE_APP_API_URL
   : isProd
-    ? 'https://api.cualbondi.com.ar/v3'
-    : 'http://localhost:8080/api/v3'
+    ? '//api.cualbondi.com.ar/v3'
+    : '//localhost:8080/api/v3'
 
 export const SENTRY_URL = isProd
   ? 'https://67b7a80534a5442cb7dabed8033a7d65@sentry.cualbondi.com.ar/5'
